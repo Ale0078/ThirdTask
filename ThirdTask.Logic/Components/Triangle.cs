b6cc56b8b6cd52f610eedfe1b0d9bee1e3db72d8
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-06 09:48:55 +0100

    ThirdTask.Logic was changed. Class Triangle was added to the path Components/Interface

---
+++ ThirdTask.Logic/Components/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using ThirdTask.Logic.Components.Interfaces;
+
+namespace ThirdTask.Logic.Components
+{
+    public class Triangle : IFigure
+    {
+        public double FirstSide { get; init; }
+        public double SecondSide { get; init; }
+        public double ThirdSide { get; set; }
+
+        public double SemiPerimeter =>
+            (FirstSide + SecondSide + ThirdSide) / 2.0;
+
+        public double Suaqre =>
+            Math.Sqrt(SemiPerimeter * (SemiPerimeter - FirstSide) * (SemiPerimeter - SecondSide) * (SemiPerimeter - ThirdSide));
+
+        public Triangle(double firstSide, double secondSide, double thirdSide) 
+        {
+            FirstSide = firstSide;
+            SecondSide = secondSide;
+            ThirdSide = thirdSide;
+        }
+
+        public int CompareTo(IFigure figure) =>
+            SemiPerimeter.CompareTo(figure.Suaqre);
+    }
+}

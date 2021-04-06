4a23bc75eeedf9b8e6adc7ce7b07daa271b7e6aa
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-06 09:54:47 +0100

    ThirdTask.Logic was changed. Abstract Class FigureBuilder was added to the path Components/Builders/Abstracts

---
+++ ThirdTask.Logic/Components/Builders/Abstracts/FigureBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using ThirdTask.Logic.Components.Interfaces;
+
+namespace ThirdTask.Logic.Components.Builders.Abstracts
+{
+    public abstract class FigureBuilder
+    {
+        public List<double> Sides { get; protected set; }
+
+        public FigureBuilder(params double[] sides) =>
+            Sides.AddRange(sides);
+
+        public abstract IFigure Create();
+    }
+}

30138461936380c9ed66c2127b517f7f0cbd8fe1
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-06 10:36:38 +0100

    ThirdTask was changed. Class FigureView was added to the path Views

---
+++ ThirdTask/Views/FigureView.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+using ThirdTask.Logic.UserInterface.Abstracts;
+
+namespace ThirdTask.Views
+{
+    public class FigureView : View
+    {
+        public FigureView(Model figureViewModel) : base(figureViewModel) 
+        {
+        }
+
+        public override void Display()
+        {
+            foreach (var item in FigureContainer.Figures) 
+            {
+                WriteLine(item);
+            }
+        }
+    }
+}

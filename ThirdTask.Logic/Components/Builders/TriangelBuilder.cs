278eee5512ed26fe9f1b32d96547538f9fc8a17c
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-06 09:58:28 +0100

    ThirdTask.Logic was changed. Class TriangleBuilder was added to the path Components/Builders

---
+++ ThirdTask.Logic/Components/Builders/TriangelBuilder.cs
@@ -0,0 +1,21 @@
+﻿using ThirdTask.Logic.Components;
+using ThirdTask.Logic.Components.Interfaces;
+using ThirdTask.Logic.Components.Builders.Abstracts;
+
+namespace ThirdTask.Logic.Components.Builders
+{
+    public class TriangelBuilder : FigureBuilder
+    {
+        public TriangelBuilder(params double[] sides) : base(sides) 
+        {
+        }
+
+        public override IFigure Create()
+        {
+            return new Triangle(
+                firstSide: Sides[0],
+                secondSide: Sides[1],
+                thirdSide: Sides[2]);
+        }
+    }
+}

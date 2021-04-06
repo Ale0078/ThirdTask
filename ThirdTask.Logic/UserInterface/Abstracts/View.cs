118a43851b300afb471472b63cff48e383ff19d7
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-06 10:14:25 +0100

    ThirdTask.Logic was changed. Abstract Class View was added to the path UserInterface/Abstracts

---
+++ ThirdTask.Logic/UserInterface/Abstracts/View.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ThirdTask.Logic.UserInterface.Abstracts
+{
+    public abstract class View
+    {
+        public Model FigureContainer { get; }
+
+        public View(Model figureContainer) =>
+            FigureContainer = figureContainer;
+
+        public abstract void Display();
+    }
+}

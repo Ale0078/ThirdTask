db0b6e99f78c2da6ba1fc50be907a6e012b6a779
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-06 10:32:11 +0100

    ThirdTask was changed. Class FigureViewModel was added to the path Models

---
+++ ThirdTask/Models/FigureViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ThirdTask.Logic.Components.Interfaces;
+using ThirdTask.Logic.UserInterface.Abstracts;
+
+namespace ThirdTask.Models
+{
+    public class FigureViewModel : Model
+    {
+        public FigureViewModel() : base() 
+        {
+        }
+
+        public override void AddFigure(IFigure figure) =>
+            Figures.Add(figure);
+    }
+}
